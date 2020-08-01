058bc11b8445da03527b9a3e173b3a8f911cfc61
Crawbyte <cristian.roso@gmail.com>
2020-08-01 17:00:44 -0400

    Translation

--- main.R
+++ main.R
@@ -1,11 +1,12 @@
 # Getting source data
-        # DP16: Casos por genero y grupo de edad
+        # DP16: Cases by gender and age group
         confirmedURL <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto16/CasosGeneroEtario_T.csv"
         confirmedData <- download.file(confirmedURL, destfile = "./data/dataconfirmed.csv")
         library(data.table)
         confirmedData <- fread("./data/dataconfirmed.csv")
 
-        # DP10: Fallecidos con COVID-19 por grupo de edad
+        # DP10: COVID-19 deaths by age group
         comorbidityURL <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto10/FallecidosEtario_T.csv"
         comorbidityData <- download.file(comorbidityURL, destfile = "./data/comorbidity.csv")
         comorbidityData <- fread("./data/comorbidity.csv")
+        
